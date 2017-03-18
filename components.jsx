f38846662008442e24bc76861447687fb68636ff
aso4 <albertyso@gmail.com>
2017-03-17 23:31:20 -0400

    move js to jsx file

--- components.jsx
+++ components.jsx
@@ -1,11 +1,15 @@
+
 class StoryBox extends React.Component { // upper camel case. inherits from React base class.
   render() { // all components need this.
-    return ( <div>Story Box</div>); // don't need quotes bc 'jsx'
+    return (
+      <div>
+        <h3>Stories App</h3>
+        <p className="lead">Sample paragraph</p>
+      </div>
+    );
   }
 }
 
 ReactDOM.render( // takes two arguments. 1) invoke StoryBox 2) target container element to be rendered to
   <StoryBox />, document.getElementById('story-app')
 );
-
-export default StoryBox;
