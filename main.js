2e9297589d4173872488dc6a10efd45827eacac6
aso4 <albertyso@gmail.com>
2017-03-18 01:23:40 -0400

    add starter code

--- main.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import StoryBox from './components.js';
-
-ReactDOM.render(<StoryBox />, document.getElementById('story-app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,26 @@
-var path = require('path');
-
-var config = {
-   entry: './main.js', // webpack entry point
-
-   output: {
-      path: path.join( __dirname, './dist/'),
-      filename: 'index.js',
-   },
-
-   devServer: {
-      inline: true,
-      port: 8080 // set devport to 808
-   },
-
-   module: {
-      loaders: [
-         {
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-
-            // set babel loaders to search for js files and use
-            // es2015 and react presets installed with npm
-            query: {
-               presets: ['es2015', 'react']
-            }
-         }
-      ]
-   }
-}
-
-module.exports = config;
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
+};
